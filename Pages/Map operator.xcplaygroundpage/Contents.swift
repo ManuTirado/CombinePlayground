f989b21c10d819c429d419b4218b8c3e84108263
manueltirado <manueltirado@192.168.1.132>
2024-05-21 20:22:05 +0200

    map operator

--- Pages/Map operator.xcplaygroundpage/Contents.swift
+++ Pages/Map operator.xcplaygroundpage/Contents.swift
@@ -3,6 +3,17 @@
 import Foundation
 import Combine
 
+// MARK: Map int from publisher to string of subscriber
 
+let intPublisher = PassthroughSubject<Int, Never>()
+
+intPublisher
+    .map { String($0) }
+    .sink { value in
+        print("Value: \(value)")
+    }
+
+intPublisher.send(18)
+intPublisher.send(45)
 
 //: [Next](@next)
